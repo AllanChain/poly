6256e93ccf1d882f4f7bf7aec3d5649afdc7a601
AllanChain <txsmlf@gmail.com>
2018-07-24 12:01:30 +0800

    first trial to change poly into a function.

--- poly/core.py
+++ poly/core.py
@@ -38,57 +38,54 @@ class commonPoly:
         num=bools.count(True)
         ans=True if num%2==1 else False
         return ans
-class poly(commonPoly):
-    def __init__(self,n,r=None,size=None,topleft=(0,0),center=None,lie=True):
-        '''def __init__(self,n,r=None,size=None,topleft=(0,0),lie=True)
-n is the num of size of the poly;
-'''
-        assert n<=20,'n is too large'
-        assert n>=3,'n is too small'
-        assert int(n)==n,'n must be an integer'
-        assert isinstance(r, (int, float)) or isinstance(size, (int, float)),\
-              'The r or size must be an number'
-        if size!=None and r==None:
-            r=(size/2)/sin(pi/n)
-        else:
-            size=r*sin(pi/n)*2
-        
-            #topleft=(size/2)/sin(pi/n)
-        if n % 2==0:#if it's an even one
-            if n%4==2:
-                rect=(r*2,r*2*cos(pi/n)) if lie else (r*2*cos(pi/n),r*2)
-                    #the rect that holds the poly
-            else:
-                rect=(r*2*cos(pi/n),r*2*cos(pi/n)) if lie else (r*2,r*2)
-            sx,sy=rect[0]/2,rect[1]/2
-        else:#an odd one
-            rect=(r*sin((n//2)*pi/n)*2,r*cos(pi/n)+r)
-            sx=r*sin((n//2)*pi/n)
-            sy=r if lie else r*cos(pi/n)
-        if center!=None:
-            topleft=(center[0]-rect[0]/2,center[1]-rect[1]/2)
-            #以center为准，可能和圆心不重合！！
-        x,y=topleft
-        sx+=x
-        sy+=y
-        sang=pi/n if lie else 0#start angle
-        step=pi*2/n
-        points=[]
-        for i in range(n):
-            ang=i*step+sang
-            points.append((int(sx+sin(ang)*r),int(sy+cos(ang)*r)))#由下方或偏右逆时针编号
-        self.topleft,self.points=topleft,points
-        self.rect=rectangle.Rect(topleft+rect)
-        self.n,self.r,self.size=n,r,size
-        self.center=(sx,sy)
-        #print(sx)
-        return
     def copy_and_move(self,d):
         dx,dy=d
         ox,oy=self.topleft
         return poly(n=self.n,r=self.r,topleft=(ox+dx,oy+dy))
     def __str__(self):
         return 'A poly with %d edge and %d r'%(self.n,self.r)
+
+def poly(n,r=None,size=None,topleft=(0,0),center=None,lie=True):
+    assert n<=20,'n is too large'
+    assert n>=3,'n is too small'
+    assert int(n)==n,'n must be an integer'
+    assert isinstance(r, (int, float)) or isinstance(size, (int, float)),\
+          'The r or size must be an number'
+    if size!=None and r==None:
+        r=(size/2)/sin(pi/n)
+    else:
+        size=r*sin(pi/n)*2
+        #topleft=(size/2)/sin(pi/n)
+    if n % 2==0:#if it's an even one
+        if n%4==2:
+            rect=(r*2,r*2*cos(pi/n)) if lie else (r*2*cos(pi/n),r*2)
+                #the rect that holds the poly
+        else:
+            rect=(r*2*cos(pi/n),r*2*cos(pi/n)) if lie else (r*2,r*2)
+        sx,sy=rect[0]/2,rect[1]/2
+    else:#an odd one
+        rect=(r*sin((n//2)*pi/n)*2,r*cos(pi/n)+r)
+        sx=r*sin((n//2)*pi/n)
+        sy=r if lie else r*cos(pi/n)
+    if center!=None:
+        topleft=(center[0]-rect[0]/2,center[1]-rect[1]/2)
+        #以center为准，可能和圆心不重合！！
+    x,y=topleft
+    sx+=x
+    sy+=y
+    sang=pi/n if lie else 0#start angle
+    step=pi*2/n
+    points=[]
+    for i in range(n):
+        ang=i*step+sang
+        points.append((int(sx+sin(ang)*r),int(sy+cos(ang)*r)))#由下方或偏右逆时针编号
+    self=commonPoly(points)
+    self.topleft=topleft
+    self.rect=rectangle.Rect(topleft+rect)
+    self.n,self.r,self.size=n,r,size
+    self.center=(sx,sy)
+    #print(sx)
+    return self
    
        
 def main():
