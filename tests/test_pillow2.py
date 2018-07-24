ff70e0627f485326540bb96949d692138022c8c9
AllanChain <txsmlf@gmail.com>
2018-07-24 14:35:12 +0800

    add pillow demo 2 to test sigle poly

---
+++ tests/test_pillow2.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageDraw,ImageFont
+from poly import poly,PolyGroup
+
+a=poly(n=6,r=40,topleft=(0,0),lie=True)
+im = Image.new("RGBA",(100,100),(0,0,0))
+font=ImageFont.load_default()
+draw = ImageDraw.Draw(im)
+j=0
+draw.polygon(a.points,outline=(0,255,0),fill=(0,0,255))
+for p in a.points:
+    print(p)
+    draw.text(p,str(j),font=font)
+    j+=1
+draw.rectangle(a.rect.ixyxy)
+im.save('test_img2.png', "PNG")
